88be1639c0b222d4d9c6b19d0c3356ff77f64956
Ingo Schommer <ingo@silverstripe.com>
2017-03-16 12:14:05 +1300

    Reko API interaction mock
    Not tested against the actual API yet

--- server/handlers/upload-photo.js
+++ server/handlers/upload-photo.js
@@ -5,25 +5,70 @@ AWS.config.update({
   region: 'ap-southeast-2'
 });
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+// TODO Determine from API Gateway stage
+const stage = 'uat';
 
-exports.handler = function (event, context) {
-  const user = {
-    uuid: Math.random() < 0.5 ? '4278f790-070e-11e7-98f6-d72bef2d4021' : '7b545370-070e-11e7-aa98-35a1c7d1e25c'
-  };
-
+/**
+ * Analyses a photo for faces. The photo was previously uploaded
+ * to a defined S3 bucket by the same user.
+ *
+ * @param {String} objectKey S3 object (not a URL)
+ * @return {Promise} Returning a FaceId
+ */
+var indexFace = (objectKey) => {
+  var rekognitionClient = new AWS.Rekognition();
   var params = {
-    TableName: 'echt.uat.photos',
+    Image: {
+     S3Object: {
+       // Ensure photos can only be selected from a location we control
+       Bucket: `echt.${stage}`,
+       Name: objectKey
+     }
+    }
+  };
+  return rekognitionClient.indexFaces(params).promise().then((response) => {
+    // TODO Fail when more than one face detected
+    // TODO Limit multi face failure to similar bounding boxes,
+    // avoid failing when photo captures people in the background
+    // TODO Fail when face is detected with low confidence
+    return response.data.FaceRecords[0].Face.FaceId;
+  });
+}
+
+/**
+ * @param {Object} user
+ * @param {String} objectKey S3 object key (not a URL)
+ * @param {String} faceId AWS Rekognition detected face
+ * @return {Promise} Returning doc info
+ */
+var storeDoc = (user, objectKey, faceId) => {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: `echt.${stage}.photos`,
     Item: {
-      'uuid': uuid.v1(),
-      'userId': user.uuid,
-      'info': {
-        'test': 'This is a test post'
-      }
+      uuid: uuid.v1(),
+      userId: user.uuid,
+      faceId: faceId,
     }
   };
 
-  return docClient.put(params).promise().then((data) => {
-    return data;
+  return docClient.put(params).promise().then((response) => {
+    return response.data;
   });
+}
+
+exports.handler = function (request) {
+  const photoKey = request.body.photoKey;
+
+  // TODO Check user existence in database
+  const user = request.body.user;
+
+  return indexFace(photoKey)
+    .then(faceData => {
+      return storeDoc(user, photoKey, faceData);
+    })
+    .then(() => {
+      // TODO Return object information in API response
+      return {status: 'ok'};
+    });
 };
--- server/tests/upload-photo-test.js
+++ server/tests/upload-photo-test.js
@@ -1,10 +1,76 @@
+const proxyquire = require('proxyquire');
 const test = require('tape');
-const uploadPhoto = require('../handlers/upload-photo');
+const sinon = require('sinon');
 
-test('upload', function (t) {
-  t.plan(1);
+const configStub = {
+  update: sinon.stub(),
+};
 
-  uploadPhoto.handler().then((result) => {
+const dynamoPutStub = sinon.stub().returns({
+  promise: () => Promise.resolve({
+    data: {
+      Attributes: {}
+    }
+  }),
+});
+
+const dynamoStub = {
+  DocumentClient: function DocumentClient() {
+    this.put = dynamoPutStub;
+  },
+};
+
+const rekognitionIndexFacesStub = sinon.stub().returns({
+  promise: () => Promise.resolve({
+    data: {
+      FaceRecords: [
+        {
+          Face: {
+            BoundingBox: {
+              Height: 0.3,
+              Left: 0.3,
+              Top: 0.3,
+              Width: 0.3
+            },
+            Confidence: 99.99,
+            FaceId: "my-faceid",
+            ImageId: "my-imageid",
+          },
+        },
+      ]
+    },
+  }),
+});
+
+const rekognitionStub = function Rekognition() {
+  this.indexFaces = rekognitionIndexFacesStub;
+};
+
+const awsStub = {
+  config: configStub,
+  DynamoDB: dynamoStub,
+  Rekognition: rekognitionStub,
+};
+
+const uploadPhoto = proxyquire('../handlers/upload-photo', {
+  'aws-sdk': awsStub,
+});
+
+test('stores when called with valid object and a detected face', function (t) {
+  const request = {
+    body: {
+      photoKey: 'my-photo-key.jpg',
+      user: {
+        uuid: 'my-uuid'
+      }
+    },
+  };
+  uploadPhoto.handler(request).then((result) => {
+    t.ok(dynamoPutStub.calledOnce);
+    const putCall = dynamoPutStub.getCall(0);
+    t.equal(putCall.args[0].Item.userId, 'my-uuid');
+    t.equal(putCall.args[0].Item.faceId, 'my-faceid');
     t.ok(result);
+    t.end();
   });
 });
