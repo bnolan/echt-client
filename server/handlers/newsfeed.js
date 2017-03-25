edff46061305153b55bf589beaa188a0a28eb949
Ben Nolan <bnolan@gmail.com>
2017-03-26 09:45:10 +1000

    Added lodash, sorting newsfeed.

--- server/handlers/newsfeed.js
+++ server/handlers/newsfeed.js
@@ -1,6 +1,7 @@
 var AWS = require('aws-sdk');
 const getStage = require('../helpers/get-stage');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 // TODO Move to environment var
 // Region needs to be supported by Rekognition (and match the S3 bucket)
@@ -29,7 +30,9 @@ exports.handler = (request) => {
   return docClient.scan(params).promise().then((data) => {
     return {
       success: true,
-      items: data.Items
+      items: _.sortBy(data.Items, (i) => {
+        return 0 - new Date(i.created_at).getTime()
+      })
     };
   });
 };
