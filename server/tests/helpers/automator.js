a994aa7eaca6ba2ef5daef4c3049b2a99de4b69c
Ben Nolan <bnolan@gmail.com>
2017-03-30 21:15:27 +1300

    Add helpers.

---
+++ server/tests/helpers/automator.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const app = require('../../app');
+
+class Automator {
+  get (path, queryString, headers, callback) {
+    app.proxyRouter({
+      requestContext: {
+        resourcePath: path,
+        httpMethod: 'GET'
+      },
+      queryStringParameters: queryString,
+      headers: headers
+    }, {
+      done: function (something, response) {
+        callback(JSON.parse(response.body));
+      }
+    });
+  }
+
+  put (path, jsonBody, headers, callback) {
+    let headersWithDefaults = Object.assign({'content-type': 'application/json'}, headers);
+    app.proxyRouter({
+      requestContext: {
+        resourcePath: path,
+        httpMethod: 'PUT'
+      },
+      headers: headersWithDefaults,
+      body: JSON.stringify(jsonBody)
+    }, {
+      done: function (something, response) {
+        callback(JSON.parse(response.body));
+      }
+    });
+  }
+
+  post (path, jsonBody, headers, callback) {
+    let headersWithDefaults = Object.assign({'content-type': 'application/json'}, headers);
+    app.proxyRouter({
+      requestContext: {
+        resourcePath: path,
+        httpMethod: 'POST'
+      },
+      headers: headersWithDefaults,
+      body: jsonBody
+    }, {
+      done: function (something, response) {
+        callback(JSON.parse(response.body));
+      }
+    });
+  }
+}
+
+module.exports = Automator;
