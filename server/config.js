6fec7af901e62c2e299308bae13593c5823874ac
Ingo Schommer <ingo@silverstripe.com>
2017-04-25 20:40:23 +1200

    Split out server components

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  awsRegion: 'us-west-2',
-  defaultStages: ['test', 'uat', 'prod'],
-  tapeTestStage: 'test'
-};
--- server/tests/bootstrap.js
+++
@@ -1,4 +0,0 @@
-process.on('unhandledRejection', (reason, p) => {
-  console.log(JSON.stringify(reason));
-  console.log(JSON.stringify(p));
-});
