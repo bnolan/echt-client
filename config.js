471d20aaf2384372c17ac114bbe665a7f1a0f9a6
Ingo Schommer <me@chillu.com>
2017-09-15 22:19:28 +1200

    Reverted config.js endpoints

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   endpoint: {
-    uat: 'http://localhost:3000',
-    // uat: 'https://xypqnmu05f.execute-api.us-west-2.amazonaws.com/uat',
+    // uat: 'http://localhost:3000',
+    uat: 'https://xypqnmu05f.execute-api.us-west-2.amazonaws.com/uat',
     prod: 'https://xypqnmu05f.execute-api.us-west-2.amazonaws.com/prod'
   },
   fixture: null
