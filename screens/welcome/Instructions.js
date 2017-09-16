1cf54b6674a8b79d2dee0ed38620857ba2da6970
Ingo Schommer <me@chillu.com>
2017-09-16 17:24:52 +1200

    Fixed instruction styling

--- screens/welcome/Instructions.js
+++ screens/welcome/Instructions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Animatable from 'react-native-animatable';
 import store from '../../state/store';
 import styles from '../styles';
 
@@ -9,8 +10,10 @@ import {
   Icon,
   Container,
   Header,
-  Content,
-  Button
+  Col,
+  Button,
+  Grid,
+  Row
 } from 'native-base';
 
 export default class Instructions extends React.Component {
@@ -32,33 +35,47 @@ export default class Instructions extends React.Component {
             <Title>Turn a selfie into a friend</Title>
           </Body>
         </Header>
-        <Content style={styles.padding15}>
-          <Text style={styles.textParagraph}>
-            Your friends see
-            photos you take in Echt.
-          </Text>
-          <Text style={styles.textParagraph}>
-            To add a friend, switch to the front facing camera
-            and take a selfie with your friend.
-          </Text>
-          <Text style={styles.textParagraph}>
-            Echt will recognize your friend
-            and send them a friend request.
-          </Text>
-          <Text style={styles.textParagraph}>
-            If your friend doesn't use Echt, you send
-            them an invitation, and when they sign up, we'll
-            add them.
-          </Text>
-          <Button
-            block
-            primary
-            onPress={() => this.finishSignup()}
-          >
-            <Icon name='arrow-dropright' />
-            <Text>Start</Text>
-          </Button>
-        </Content>
+        <Grid>
+          <Row style={[styles.margin15, {flexDirection: 'column'}]}>
+            <Text style={styles.textParagraph}>
+              Your friends see
+              photos you take in Echt.
+            </Text>
+            <Text style={styles.textParagraph}>
+              To add a friend, switch to the front facing camera
+              and take a selfie with your friend.
+            </Text>
+            <Text style={styles.textParagraph}>
+              Echt will recognize your friend
+              and send them a friend request.
+            </Text>
+            <Text style={styles.textParagraph}>
+              If your friend doesn't use Echt, you send
+              them an invitation, and when they sign up, we'll
+              add them.
+            </Text>
+          </Row>
+
+          <Row style={[ styles.margin15, styles.flex0 ]}>
+            <Grid>
+              <Col>
+                <Animatable.View
+                  style={styles.container}
+                  animation='fadeIn'
+                  delay={500}>
+                  <Button
+                    block
+                    primary
+                    onPress={() => this.finishSignup()}
+                  >
+                    <Icon name='arrow-dropright' />
+                    <Text>Start</Text>
+                  </Button>
+                </Animatable.View>
+              </Col>
+            </Grid>
+          </Row>
+        </Grid>
       </Container>
     );
   }
